63fd1b3687daa69372bd564f22be4c3e04740178
Simon Luijk <simon@simonluijk.com>
2015-07-29 13:20:10 +0000

    Major fork to allow to use with etcd
    This also removes the possibility of updating for all services
    at the same time.

--- services.py
+++ services.py
@@ -17,9 +17,12 @@ import argparse
 import logging
 import os
 import re
+import json
 import boto
 import boto.ec2
 import boto.route53
+import requests
+from etcd.client import Client
 from time import sleep
 
 
@@ -31,7 +34,6 @@ route53 = boto.route53.connect_to_region(region)
 
 logging.basicConfig(format='%(asctime)s %(message)s',
                     datefmt='%Y/%m/%d/ %I:%M:%S %p')
-log = logging.info
 
 if 'ECS_CLUSTER' in os.environ:
     cluster = os.environ['ECS_CLUSTER']
@@ -42,33 +44,11 @@ else:
     cluster = None
 
 
-def get_task_definition_arns():
-    """Request all API pages needed to get Task Definition ARNS."""
-    next_token = []
-    arns = []
-    while next_token is not None:
-        detail = ecs.list_task_definitions(next_token=next_token)
-        detail = detail['ListTaskDefinitionsResponse']
-        detail = detail['ListTaskDefinitionsResult']
-        arns.extend(detail['taskDefinitionArns'])
-        next_token = detail['nextToken']
-    return arns
-
-
-def get_task_definition_families():
-    """Ignore duplicate tasks in the same family."""
-    arns = get_task_definition_arns()
-    families = {}
-    for arn in arns:
-        match = pattern_arn.match(arn)
-        if match:
-            groupdict = match.groupdict()
-            families[groupdict['family']] = True
-    return families.keys()
-
-
 def get_task_arns(family):
-    """Get the ARN of running task, given the family name."""
+    """
+    Get the ARN of running task, given the family name.
+    """
+
     response = ecs.list_tasks(cluster=cluster, family=family)
     arns = response['ListTasksResponse']['ListTasksResult']['taskArns']
     if len(arns) == 0:
@@ -76,30 +56,24 @@ def get_task_arns(family):
     return arns
 
 
-def get_task_container_instance_arn(task_arn):
-    """Get the ARN for the container instance a give task is running on."""
-    response = ecs.describe_tasks(task_arn, cluster=cluster)
-    response = response['DescribeTasksResponse']
-    return response['DescribeTasksResult']['tasks'][0]['containerInstanceArn']
 
+def get_ec2_interface(container_instance_arn):
+    """
+    Get the ec2 interface from an container instance ARN. 
+    """
 
-def get_container_instance_ec2_id(container_instance_arn):
-    """Id the EC2 instance serving as the container instance."""
-    detail = ecs.describe_container_instances(
-        container_instances=container_instance_arn, cluster=cluster)
-    detail = detail['DescribeContainerInstancesResponse']
-    detail = detail['DescribeContainerInstancesResult']['containerInstances']
-    return detail[0]['ec2InstanceId']
+    response = ecs.describe_container_instances(container_instance_arn, cluster=cluster)
+    ec2_instance_id = response['DescribeContainerInstancesResponse'] \
+        ['DescribeContainerInstancesResult']['containerInstances'] \
+        [0]['ec2InstanceId']
 
-
-def get_ec2_interface(ec2_instance_id):
-    """Get the primary interface for the given EC2 instance."""
-    return ec2.get_all_instances(filters={
-        'instance-id': ec2_instance_id})[0].instances[0].interfaces[0]
+    response = ec2.get_all_instances(filters={'instance-id': ec2_instance_id})
+    return response[0].instances[0].interfaces[0]
 
 
 def get_zone_for_vpc(vpc_id):
-    """Identify the Hosted Zone for the given VPC.
+    """
+    Identify the Hosted Zone for the given VPC.
 
     Assumes a 1 to 1 relationship.
 
@@ -111,6 +85,7 @@ def get_zone_for_vpc(vpc_id):
     one VPC. (But, why would you expect internal DNS for 2 different private
     networks to be the same anyway?)
     """
+
     response = route53.get_all_hosted_zones()['ListHostedZonesResponse']
     for zone in response['HostedZones']:
         zone_id = zone['Id'].split('/')[-1]
@@ -122,58 +97,48 @@ def get_zone_for_vpc(vpc_id):
             pass
 
 
-def get_service_info(family):
+def get_service_info(service_name):
     info = {
-        "vpc_id": None,
-        "family": family,
-        "name": family[:-8],
-        "service_ips": []
+        "name": service_name,
+        "tasks": []
     }
 
-    if family[-8:] != '-service':
-        log('    (Found non-service {0})'.format(family))
-        return None
+    if service_name[-8:] == '-service':
+        info['name'] = service_name[:-8]
 
-    log('{0} service found'.format(family))
-
-    service_arns = get_task_arns(family)
-    if not service_arns:
-        log('{0} is not running'.format(family))
+    task_arns = get_task_arns(service_name)
+    if not task_arns:
+        logging.info('{0} is NOT RUNNING'.format(service_name))
         return None
     else:
-        log('{0} is RUNNING'.format(family))
+        logging.info('{0} is RUNNING'.format(service_name))
 
-        for service_arn in service_arns:
-            container_arn = get_task_container_instance_arn(service_arn)
-            ec2_instance_id = get_container_instance_ec2_id(container_arn)
-            ec2_interface = get_ec2_interface(ec2_instance_id)
+        data = ecs.describe_tasks(task_arns, cluster=cluster)
+        tasks = data['DescribeTasksResponse']['DescribeTasksResult']['tasks']
+        for task in tasks:
+            interface = get_ec2_interface(task['containerInstanceArn'])
+            task_info = {
+                'ip': interface.private_ip_address,
+                'ports': {}
+            }
 
-            info['vpc_id'] = ec2_interface.vpc_id
-            info['service_ips'].append(ec2_interface.private_ip_address)
+            for container in task['containers']:
+                for port in container['networkBindings']:
+                    if port['protocol'] == 'tcp':
+                        task_info['ports'][port['containerPort']] = port['hostPort']
+
+            info['tasks'].append(task_info)
+            info['vpc_id'] = interface.vpc_id
 
     return info
 
 
-def get_info():
-    """Get all needed info about running services."""
-    info = {'services': [], 'network': {'cluster': cluster}}
-    families = get_task_definition_families()
-    for family in families:
-        service_info = get_service_info(family)
-        if not service_info:
-            continue
-        # No need to get common network info on each loop over tasks
-        if 'vpc_id' not in info['network']:
-            info['network'].update(get_zone_for_vpc(service_info["vpc_id"]))
-            info['network']['vpc_id'] = service_info["vpc_id"]
-        info['services'].append(service_info)
-    return info
+def update_dns(zone_id, zone_name, service_name, service_ips, ttl=20):
+    """
+    Insert or update DNS record.
+    """
 
-
-def dns(zone_id, zone_name, service_name, service_ips, ttl=20):
-    """Insert or update DNS record."""
     host_name = '.'.join([service_name, zone_name])
-
     record_set = boto.route53.record.ResourceRecordSets(route53, zone_id)
     record = record_set.add_change('UPSERT', host_name, 'A', ttl)
     for service_ip in service_ips:
@@ -182,56 +147,55 @@ def dns(zone_id, zone_name, service_name, service_ips, ttl=20):
     return record_set
 
 
-def update_services(service_names=[], verbose=False):
-    """Update DNS to allow discovery of properly named task definitions.
-
-    If service_names are provided only update those services.
-    Otherwise update all.
+def update_service(service_name, method):
     """
-    info = get_info()
-    for service in info['services']:
-        if (service_names and
-                service['family'] not in service_names and
-                service['name'] not in service_names):
-            continue
+    Update DNS to allow discovery of properly named task definitions.
 
-        if verbose:
-            log('Registering {0}.{1} as {2}'.format(
-                service['name'], info['network']['zone_name'],
-                ','.join(service['service_ips'])))
+    """
 
-        dns(info['network']['zone_id'], info['network']['zone_name'],
-            service['name'], service['service_ips'])
+    info = get_service_info(service_name)
+    if not info:
+        return None
+
+    if method == 'dns':
+        network = get_zone_for_vpc(info["vpc_id"])
+        ips = [t['ip'] for t in info['tasks']]
+        logging.info('Registering {0}.{1} as {2}'.format(
+                     info['name'], network['zone_name'], ','.join(ips)))
+
+        update_dns(network['zone_id'], network['zone_name'],
+                   info['name'], ips)
+    elif method == 'etcd':
+        host = requests.get("http://169.254.169.254/latest/meta-data/local-ipv4").content
+        client = Client(host=host, port=4001)
+        client.node.set('/test', json.dumps(info))
+        print client.node.get('/test').node.value
 
 
-def cli():
-    """Used by entry_point console_scripts."""
+def main():
+    """
+    Main function that handles running the command.
+    """
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('service_names', nargs='*',
+    parser.add_argument('service_name', nargs=1,
                         help='list of services to start')
+    parser.add_argument('method', nargs=1,
+                        help='method of registering service')
     parser.add_argument('-q', '--quiet', action='store_true',
                         help='suppress output')
     parser.add_argument('-r', '--rerun', action='store_true',
                         help='run again after a 60 second pause')
     args = parser.parse_args()
+
     if not args.quiet:
         logging.getLogger().setLevel(logging.INFO)
-    update_services(args.service_names, True)
+
+    update_service(args.service_name[0], args.method[0])
     if args.rerun:
         sleep(60)
-        update_services(args.service_names, True)
+        update_service(args.service_name[0], args.method[0])
 
-pattern_arn = re.compile(
-    'arn:'
-    '(?P<partition>[^:]+):'
-    '(?P<service>[^:]+):'
-    '(?P<region>[^:]*):'   # region is optional
-    '(?P<account>[^:]*):'  # account is optional
-    '(?P<resourcetype>[^:/]+)([:/])'
-    '(?P<resource>('
-        '(?P<family>[^:]+):'     # noqa
-        '(?P<version>[^:]+)|.*'  # noqa
-    '))')
 
 if __name__ == '__main__':
-    cli()
+    main()
