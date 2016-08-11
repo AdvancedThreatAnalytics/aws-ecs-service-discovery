b7f5a080afd24256b5ac36f54fa3bfc24651b20d
Ross Urban <ross.urban@gmail.com>
2016-08-11 13:50:04 -0400

    Changes to use boto3 + support addition of a cname (optional)

--- register.py
+++ register.py
@@ -16,181 +16,248 @@ For example:
 import argparse
 import logging
 import os
-import re
-import json
-import boto
-import boto.ec2
-import boto.route53
-import requests
-from etcd.client import Client
+import boto3
 from time import sleep
 
-
-region = os.environ.get('ECS_REGION', 'us-east-1')
-ecs = boto.connect_ec2containerservice(
-        host='ecs.{0}.amazonaws.com'.format(region))
-ec2 = boto.ec2.connect_to_region(region)
-route53 = boto.route53.connect_to_region(region)
-
-logging.basicConfig(format='%(asctime)s %(message)s',
-                    datefmt='%Y/%m/%d/ %I:%M:%S %p')
-
-if 'ECS_CLUSTER' in os.environ:
-    cluster = os.environ['ECS_CLUSTER']
-elif os.path.exists('/etc/ecs/ecs.config'):
-    pat = re.compile(r'\bECS_CLUSTER\b\s*=\s*(\w*)')
-    cluster = pat.findall(open('/etc/ecs/ecs.config').read())[-1]
-else:
-    cluster = None
+from botocore.exceptions import ClientError
 
 
-def get_task_arns(family):
+def find(service_name, container_name, cluster='default', private=True):
     """
-    Get the ARN of running task, given the family name.
+    finds the IP address of the currently running tasks of the service.
+    :return None, if no tasks are running, or [list of IP addresses]
     """
+    client = boto3.client('ecs')
+    cluster = os.environ.get('CLUSTER', cluster)
+    ipaddresses = []
+    try:
+        response = client.list_tasks(
+            cluster=cluster, serviceName=service_name, desiredStatus='RUNNING'
+        )
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            raise ValueError(response['ResponseMetadata'])
 
-    response = ecs.list_tasks(cluster=cluster, family=family)
-    arns = response['ListTasksResponse']['ListTasksResult']['taskArns']
-    if len(arns) == 0:
-        return None
-    return arns
+        response = client.describe_tasks(cluster=cluster,
+                                         tasks=response['taskArns'])
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            raise ValueError(response['ResponseMetadata'])
+
+        container_instance_arns = []
+        for task in response['tasks']:
+            for container in task['containers']:
+                if container['name'] != container_name and \
+                        not container['name'].startswith(container_name):
+                    continue
+                if container['lastStatus'] != 'RUNNING':
+                    continue
+                container_instance_arns.append(task['containerInstanceArn'])
+
+        response = client.describe_container_instances(
+            cluster=cluster, containerInstances=container_instance_arns
+        )
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            raise ValueError(response['ResponseMetadata'])
+
+        ec2 = boto3.resource('ec2')
+        for instance in response['containerInstances']:
+            inst = ec2.Instance(instance['ec2InstanceId'])
+            if private and inst.private_ip_address is not None:
+                ipaddresses.append(inst.private_ip_address)
+            elif inst.public_ip_address is not None:
+                ipaddresses.append(inst.public_ip_address)
+
+    except (IndexError, ClientError):
+        msg = 'Service {0} Container Name: {1} not found in Cluster {2}'.format(
+            service_name, container_name, cluster
+        )
+        raise Exception(msg)
+    return ipaddresses
 
 
-
-def get_ec2_interface(container_instance_arn):
+def register(service_name, container_name, dns_entry,
+             cluster='default', private=True):
     """
-    Get the ec2 interface from an container instance ARN. 
+    register creates `A` records for a given a service, and
+    (running) container, by using find in either private or public DNS entries
+    in Route 53
     """
-
-    response = ecs.describe_container_instances(container_instance_arn, cluster=cluster)
-    ec2_instance_id = response['DescribeContainerInstancesResponse'] \
-        ['DescribeContainerInstancesResult']['containerInstances'] \
-        [0]['ec2InstanceId']
-
-    response = ec2.get_all_instances(filters={'instance-id': ec2_instance_id})
-    return response[0].instances[0].interfaces[0]
+    route53 = boto3.client('route53')
+    response = route53.list_hosted_zones(MaxItems='50')
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        raise ValueError(response['ResponseMetadata'])
 
 
-def get_zone_for_vpc(vpc_id):
-    """
-    Identify the Hosted Zone for the given VPC.
+    # Resource Records from Route53 are period terminated. So
+    # blah.threatanalytics.io shows up as blah.threatanalytics.io.
+    if not dns_entry.endswith('.'):
+        dns_entry += '.'
 
-    Assumes a 1 to 1 relationship.
+    zone_name = '.'.join(dns_entry.split('.')[1:])
+    zone = get_zone(response['HostedZones'], zone_name, private)
 
-    NOTE: There is an existing bug.
-    https://github.com/boto/boto/issues/3061
-    When that changes, I expect to have to search ['VPCs'] as a list of
-    dictionaries rather than a dictionary.  This has the unfortunate side
-    effect of not working for Hosted Zones that are associated with more than
-    one VPC. (But, why would you expect internal DNS for 2 different private
-    networks to be the same anyway?)
-    """
+    if zone is None:
+        raise AttributeError('No hosted zone for {0}, searched: {1}'.format(
+            dns_entry, zone_name
+        ))
 
-    response = route53.get_all_hosted_zones()['ListHostedZonesResponse']
-    for zone in response['HostedZones']:
-        zone_id = zone['Id'].split('/')[-1]
-        detail = route53.get_hosted_zone(zone_id)['GetHostedZoneResponse']
-        try:
-            if detail['VPCs']['VPC']['VPCId'] == vpc_id:
-                return {'zone_id': zone_id, 'zone_name': zone['Name']}
-        except KeyError:
-            pass
+    response = route53.list_resource_record_sets(
+        HostedZoneId=zone['Id'],
+        StartRecordName=dns_entry,
+        StartRecordType='A'
+    )
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        raise ValueError(response['ResponseMetadata'])
+
+    existing = []
+
+    for recordset in response['ResourceRecordSets']:
+        print('Comparing {0} and {1}'.format(recordset['Name'], dns_entry))
+        if recordset['Name'] == dns_entry:  # should hit on first
+            for rs in recordset['ResourceRecords']:
+                existing.append(rs['Value'])
+            break
+
+    new = find(service_name, container_name, cluster)
+    if set(existing) != set(new):  # set resource records
+        resource_record = {
+            'Name': dns_entry,
+            'ResourceRecords': [{'Value': address} for address in new],
+            'TTL': 20,
+            'Type': 'A'
+        }
+        response = route53.change_resource_record_sets(
+            HostedZoneId=zone['Id'],
+            ChangeBatch={
+                'Comment': 'updated by ecs register container',
+                'Changes': [
+                    {
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': resource_record
+                    }
+                ]
+            }
+        )
+
+    return (response['ResponseMetadata']['HTTPStatusCode'],
+            response['ResponseMetadata'])
 
 
-def get_service_info(service_name):
-    info = {
-        "name": service_name,
-        "tasks": []
+def register_cname(dns_entry, target, private=True):
+    route53 = boto3.client('route53')
+    response = route53.list_hosted_zones(MaxItems='50')
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        raise ValueError(response['ResponseMetadata'])
+
+    # Resource Records from Route53 are period terminated. So
+    # blah.threatanalytics.io shows up as blah.threatanalytics.io.
+    if not dns_entry.endswith('.'):
+        dns_entry += '.'
+
+    zone_name = '.'.join(dns_entry.split('.')[1:])
+    zone = get_zone(response['HostedZones'], zone_name, private)
+
+    if zone is None:
+        raise AttributeError('No hosted zone for {0}, searched: {1}'.format(
+            dns_entry, zone_name
+        ))
+
+    response = route53.list_resource_record_sets(
+        HostedZoneId=zone['Id'],
+        StartRecordName=dns_entry,
+        StartRecordType='CNAME'
+    )
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        raise ValueError(response['ResponseMetadata'])
+
+    for recordset in response['ResourceRecordSets']:
+        print('Comparing {0} and {1}'.format(recordset['Name'], dns_entry))
+        if recordset['Name'] == dns_entry:  # should hit on first
+            for rs in recordset['ResourceRecords']:
+                if rs['Value'] == target:
+                    return None # CNAME is already in desired state
+            break
+
+    resource_record = {
+        'Name': dns_entry,
+        'ResourceRecords': [{'Value': target}],
+        'TTL': 20,
+        'Type': 'CNAME'
     }
 
-    if service_name[-8:] == '-service':
-        info['name'] = service_name[:-8]
+    response = route53.change_resource_record_sets(
+        HostedZoneId=zone['Id'],
+        ChangeBatch={
+            'Comment': 'updated by ecs register container',
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': resource_record
+                }
+            ]
+        }
+    )
 
-    task_arns = get_task_arns(service_name)
-    if not task_arns:
-        logging.info('{0} is NOT RUNNING'.format(service_name))
-        return None
-    else:
-        logging.info('{0} is RUNNING'.format(service_name))
-
-        data = ecs.describe_tasks(task_arns, cluster=cluster)
-        tasks = data['DescribeTasksResponse']['DescribeTasksResult']['tasks']
-        for task in tasks:
-            interface = get_ec2_interface(task['containerInstanceArn'])
-            task_info = {
-                'ip': interface.private_ip_address,
-                'ports': {}
-            }
-
-            for container in task['containers']:
-                if container['networkBindings']:
-                    for port in container['networkBindings']:
-                        if port['protocol'] == 'tcp':
-                            task_info['ports'][port['containerPort']] = port['hostPort']
-
-            info['tasks'].append(task_info)
-            info['vpc_id'] = interface.vpc_id
-
-    return info
+    return (response['ResponseMetadata']['HTTPStatusCode'],
+            response['ResponseMetadata'])
 
 
-def update_dns(zone_id, zone_name, service_name, service_ips, ttl=20):
-    """
-    Insert or update DNS record.
-    """
+def get_zone(zones, zone_name, private):
+    # for dns_entry 'elasticsearch.internal.advancedthreatanalytics.com'
+    # extract internal.advancedthreatanalytics.com and see if we have a
+    # zone for that
+    for zone in zones:
+        if zone['Name'] != zone_name:
+            continue
+        elif zone['Config']['PrivateZone'] == private:
+            return zone
 
-    host_name = '.'.join([service_name, zone_name])
-    record_set = boto.route53.record.ResourceRecordSets(route53, zone_id)
-    record = record_set.add_change('UPSERT', host_name, 'A', ttl)
-    for service_ip in service_ips:
-        record.add_value(service_ip)
-    record_set.commit()
-    return record_set
+    return None
 
 
-def update_service(service_name, method, prefix):
+def update_service(family, cluster, container, dns, cname=None):
     """
     Update DNS to allow discovery of properly named task definitions.
-
     """
 
-    info = get_service_info(service_name)
-    if not info:
+    ips = find(family, container, cluster=cluster)
+
+    if not ips:
         return None
 
-    if method == 'dns':
-        network = get_zone_for_vpc(info["vpc_id"])
-        ips = [t['ip'] for t in info['tasks']]
+    print 'Registering {0}:{1} in cluster {2} as {3}'.format(
+        family, container, cluster, dns
+    )
+    register(family, container, dns, cluster=cluster)
 
-        logging.info('Registering {0}.{1} as {2}'.format(
-                     info['name'], network['zone_name'], ','.join(ips)))
-
-        update_dns(network['zone_id'], network['zone_name'],
-                   info['name'], ips)
-    elif method == 'etcd':
-        data = json.dumps(info['tasks'])
-        logging.info('Registering {0} as {1}'.format(
-                     info['name'], data))
-
-        host = requests.get("http://169.254.169.254/latest/meta-data/local-ipv4").content
-
-        client = Client(host=host, port=4001)
-        key = '/' + '/'.join([i for i in ['tasks', prefix, info['name']] if i])
-        client.node.set(key, data)
+    if cname:
+        print 'Registering {0} as CNAME for {1}'.format(cname, dns)
+        register_cname(dns_entry=cname, target=dns)
 
 
 def main():
     """
     Main function that handles running the command.
+
+    register    --service=portalapi \
+                --
+                --dns=somethingelse.threatanalytics.io \
+                --cluster=ATAProduction \
+                --cname=somethingelse.internal.ata.com \
+                --comment=This is some comment
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('service_name', nargs=1,
-                        help='list of services to start')
-    parser.add_argument('method', nargs=1,
-                        help='method of registering service')
-    parser.add_argument('-p', '--prefix', action='store', default=False,
-                        help='prefix when saving to etcd')
+    parser.add_argument('--family', dest='family',
+                        help='service family to register')
+    parser.add_argument('--dns', dest='dns',
+                        help='fqdn to register this service to')
+    parser.add_argument('--cluster', dest='cluster',
+                        help='cluster in which the service is deployed')
+    parser.add_argument('--cname', dest='cname', default='',
+                        help='cname to register the service to in the private'
+                             ' network')
+    parser.add_argument('--container', dest='container',
+                        help='name of the docker container we are registering')
     parser.add_argument('-q', '--quiet', action='store_true',
                         help='suppress output')
     parser.add_argument('-r', '--rerun', action='store_true',
@@ -200,10 +267,18 @@ def main():
     if not args.quiet:
         logging.getLogger().setLevel(logging.INFO)
 
-    update_service(args.service_name[0], args.method[0], args.prefix)
+    update_service(family=args.family,
+                   container=args.container,
+                   cluster=args.cluster,
+                   dns=args.dns,
+                   cname=args.cname)
     if args.rerun:
         sleep(60)
-        update_service(args.service_name[0], args.method[0], args.prefix)
+        update_service(family=args.family,
+                       container=args.container,
+                       cluster=args.cluster,
+                       dns=args.dns,
+                       cname=args.cname)
 
 
 if __name__ == '__main__':
